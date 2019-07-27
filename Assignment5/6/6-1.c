a4de72630901342fcbe1512f7e148cd1ee22ba05
wangsiping97 <wangsiping1997@gmail.com>
2019-07-27 14:48:49 +0800

    Create 6-1.c

---
+++ Assignment5/6/6-1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+    char Name[59][20] = {"jinshan", "linanning", "xujiadong", "cuiwubing", "denghua", "wangsiping", "limengxin", "qiaozhengping", "fanyingxue", "zhangjianwei", "louhongxuan", "weixianzhu", "lijiachen", "zoulinwei", "litingwei", "wujiaxin", "guozijie", "gengziyin", "yujiangyue", "xuxingchen", "wangyue", "bixiaoyu", "zhouyaoxiang", "liumin", "guoyan", "jinenze", "xuechen", "panzhaoyue", "weixiao", "xiaqiuhan", "zhanwenhao", "wujiaxiang", "yeliang", "liyanxuan", "like", "yangqi", "helizhe", "huangbotao", "houmingrong", "gaohao", "dengweihao", "zhangxuefeng", "yangzeyuan", "zhanghongsen", "xiaochangrong", "houbairu", "leichenyong", "wuzijian", "liyifan", "jilincheng", "wangtianyu", "zhuzifei", "liziyue", "shezhucheng", "chenkening", "shengyinuo", "shenyihua", "liyinwen", "linzhenghuan"};
+    for(int i = 0; i < 59; i ++){
+        for(int j = 0; j < strlen(Name[i]); j++){
+            while(Name[i][j])
+#define is(c, n)
+        is (a, 01)
+        is (b, 02)
+        is (c, 03)
+        is (d, 04)
+        is (e, 05)
+        is (f, 06)
+        is (g, 07)
+        is (h, 08)
+        is (i, 09)
+        is (j, 10)
+        is (k, 11)
+        is (l, 12)
+        is (m, 13)
+        is (n, 14)
+        is (o, 15)
+        is (p, 16)
+        is (q, 17)
+        is (r, 18)
+        is (s, 19)
+        is (t, 20)
+        is (u, 21)
+        is (v, 22)
+        is (w, 23)
+        is (x, 24)
+        is (y, 25)
+        is (z, 26)
+            ;
+        }
+        printf("%s \n", Name[i]);
+    }
+    return 0;
+}
